b33e859f42da7fe06f1e441a6e3f5e6e21fe83cb
mk2019 <unicorn.mark2019@gmail.com>
2021-05-11 22:11:18 +0800

    test style design

---
+++ src/common/header/style.js
@@ -0,0 +1,19 @@
+import styled  from 'styled-components'
+
+export const HeaderWrapper = styled.div`
+position:relative;
+height :58px;
+background: red;
+border-bottom: 1px solid #f0f0f0;
+`
+
+export const Logo = styled.a`
+background-color:green;
+height: 58px;
+position: absolute;
+top:0;
+left:0;
+display:block;
+width:100px;
+height:100%;
+`
---
+++ src/style.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle, injectGlobal } from 'styled-components'
+
+export const GlobalStyled = createGlobalStyle 
+`
+body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    background: green;
+}
+`
